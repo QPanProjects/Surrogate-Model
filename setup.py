a64d1455349cb8b4bb3be46a929eff189534602e
quanpan <quanpan302@hotmail.com>
2016-12-02 16:09:32 +0100

    Surrogate Model base.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SurrogateModel',
+    version='1.0',
+    packages=['surrogate'],
+    url='www.dailyactie.com/about/quanpan',
+    license='MIT License',
+    author='Quan Pan',
+    author_email='quanpan302@hotmail.com',
+    description='Daily Actie'
+)
---
+++ surrogate/base.py
@@ -0,0 +1,5 @@
+import os
+
+# sys.path.insert(0, os.path.abspath('.'))
+
+print os.path.abspath('..')
