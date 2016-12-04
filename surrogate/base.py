6a1eeba1d1d87dbe45053c8499484e14de9053f3
quanpan <quanpan302@hotmail.com>
2016-12-04 15:30:27 +0100

    Surrogate Model base.py

---
+++ surrogate/base.py
@@ -0,0 +1,8 @@
+"""Base classes for all estimators."""
+
+# Author: Quan Pan <quanpan302@hotmail.com>
+# License: MIT License
+# Create: 2016-12-02
+
+
+##############################################################################
