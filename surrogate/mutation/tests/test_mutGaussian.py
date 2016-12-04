4e3d341bdef67ca7444c525cd15f0fc0ea956364
quanpan <quanpan302@hotmail.com>
2016-12-04 17:11:31 +0100

    Surrogate Model mutation

--- surrogate/mutation/tests/test_mutGaussian.py
+++ surrogate/mutation/tests/test_mutGaussian.py
@@ -1,6 +1,6 @@
 from surrogate.mutation.mutGaussian import mutGaussian
 
-print '\nTest.mutation: mutPolynomimutGaussianalBounded'
+print '\nTest.mutation: mutGaussian'
 ind = [x / 10.0 for x in range(0, 10, 1)]
 print '\tInput:  ind=\t' + '\t'.join(map(str, ind)) + ''
 out = mutGaussian(individual=list(ind))
---
+++ surrogate/mutation/tests/test_mutPolynomialBounded.py
@@ -0,0 +1,7 @@
+from surrogate.mutation.mutPolynomialBounded import mutPolynomialBounded
+
+print '\nTest.mutation: mutPolynomialBounded'
+ind = [x / 10.0 for x in range(0, 10, 1)]
+print '\tInput:  ind=\t' + '\t'.join(map(str, ind)) + ''
+out = mutPolynomialBounded(individual=list(ind))
+print '\tOutput: out=\t' + '\t'.join(map(str, out)) + ''
