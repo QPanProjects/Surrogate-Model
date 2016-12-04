5957dfa8654c295a6c16f0afd4dbe0563a957ccb
quanpan <quanpan302@hotmail.com>
2016-12-04 16:55:39 +0100

    Surrogate Model mutation

---
+++ surrogate/mutation/__init__.py
@@ -0,0 +1,5 @@
+from .mutGaussian import mutGaussian
+
+__all__ = [
+    'mutGaussian'
+]
---
+++ surrogate/mutation/mutGaussian.py
@@ -0,0 +1,38 @@
+import random
+from collections import Sequence
+from itertools import repeat
+
+"""This function applies a gaussian mutation of mean *mu* and standard
+deviation *sigma* on the input individual. This mutation expects a
+:term:`sequence` individual composed of real valued attributes.
+The *prob* argument is the probability of each attribute to be mutated.
+
+:param individual: Individual to be mutated.
+:param mu: Mean or :term:`python:sequence` of means for the
+           gaussian addition mutation.
+:param sigma: Standard deviation or :term:`python:sequence` of
+              standard deviations for the gaussian addition mutation.
+:param prob: Independent probability for each attribute to be mutated.
+:returns: A tuple of one individual.
+
+This function uses the :func:`~random.random` and :func:`~random.gauss`
+functions from the python base :mod:`random` module.
+"""
+
+
+def mutGaussian(individual, mu, sigma, prob):
+    size = len(individual)
+    if not isinstance(mu, Sequence):
+        mu = repeat(mu, size)
+    elif len(mu) < size:
+        raise IndexError("mu must be at least the size of individual: %d < %d" % (len(mu), size))
+    if not isinstance(sigma, Sequence):
+        sigma = repeat(sigma, size)
+    elif len(sigma) < size:
+        raise IndexError("sigma must be at least the size of individual: %d < %d" % (len(sigma), size))
+
+    for i, m, s in zip(xrange(size), mu, sigma):
+        if random.random() < prob:
+            individual[i] += random.gauss(m, s)
+
+    return individual,
