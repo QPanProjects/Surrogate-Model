a6e008a2994822858022809d72ece8d11df18b06
quanpan <quanpan302@hotmail.com>
2016-12-04 16:29:01 +0100

    Surrogate Model crossover

--- surrogate/crossover/__init__.py
+++ surrogate/crossover/__init__.py
@@ -1,11 +1,12 @@
 from .cxOnePoint import cxOnePoint
+from .cxOrdered import cxOrdered
 from .cxPartialMatch import cxPartialyMatch
 from .cxTwoPoint import cxTwoPoint
 from .cxUniform import cxUniform
 from .cxUniformPartialMatch import cxUniformPartialMatch
 
-
 __all__ = [
     'cxOnePoint', 'cxTwoPoint', 'cxUniform',
-    'cxPartialyMatch', 'cxUniformPartialMatch'
+    'cxPartialyMatch', 'cxUniformPartialMatch',
+    'cxOrdered'
 ]
