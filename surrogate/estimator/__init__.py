723e0ff48eda7f0a56d4c8fd2a7a42836dad5c4f
quanpan <quanpan302@hotmail.com>
2016-12-06 18:14:03 +0100

    Surrogate Model estimator

--- surrogate/estimator/__init__.py
+++ surrogate/estimator/__init__.py
@@ -1,9 +1,11 @@
 from .kriging import KrigingSurrogate, FloatKrigingSurrogate
 from .nearest_neighbor import NearestNeighbor
+from .neural_network import ANNSurrogate
 from .response_surface import ResponseSurface
 
 __all__ = [
     'KrigingSurrogate', 'FloatKrigingSurrogate',
     'ResponseSurface',
-    'NearestNeighbor'
+    'NearestNeighbor',
+    'ANNSurrogate'
 ]
---
+++ surrogate/estimator/neural_network.py
@@ -0,0 +1,6 @@
+from surrogate.base import SurrogateModel
+
+
+class ANNSurrogate(SurrogateModel):
+    def __init__(self):
+        super(ANNSurrogate, self).__init__()
