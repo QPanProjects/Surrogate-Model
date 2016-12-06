dec6fda617801584509b18766cc8048d2926fb5e
quanpan <quanpan302@hotmail.com>
2016-12-06 11:17:37 +0100

    Surrogate Model examples

---
+++ examples/python_cache.py
@@ -0,0 +1,3 @@
+"""
+https://www.google.nl/search?sclient=psy-ab&client=safari&rls=en&q=python%20cache%20library&oq=&gs_l=&pbx=1&biw=1680&bih=961&dpr=2&pf=p&pdl=300&cad=cbv&bvch=u&sei=C5BGWJL4L8K2abeTnpAN
+"""
