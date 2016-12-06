7ce479d3eae644d35eedd1a5c14d250de8a4a55e
quanpan <quanpan302@hotmail.com>
2016-12-06 15:03:28 +0100

    Surrogate Model estimator

--- surrogate/estimator/__init__.py
+++ surrogate/estimator/__init__.py
@@ -1,5 +1,7 @@
 from .kriging import KrigingSurrogate, FloatKrigingSurrogate
+from .response_surface import ResponseSurface
 
 __all__ = [
-    'KrigingSurrogate', 'FloatKrigingSurrogate'
+    'KrigingSurrogate', 'FloatKrigingSurrogate',
+    'ResponseSurface'
 ]
