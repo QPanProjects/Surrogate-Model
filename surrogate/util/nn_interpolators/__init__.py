7620458f12694563065e078db56075ff0a5a27e9
quanpan <quanpan302@hotmail.com>
2016-12-06 16:55:55 +0100

    Surrogate Model NDInterp

---
+++ surrogate/util/nn_interpolators/__init__.py
@@ -0,0 +1,9 @@
+from .linear_interpolator import LinearInterpolator
+from .rbf_interpolator import RBFInterpolator
+from .weighted_interpolator import WeightedInterpolator
+
+__all__ = [
+    'LinearInterpolator',
+    'RBFInterpolator',
+    'WeightedInterpolator'
+]
