e8c557f0c4ccb01a7ee8f1d6609dfff891ebbee1
quanpan <quanpan302@hotmail.com>
2016-12-07 16:26:24 +0100

    Surrogate Model sampling

--- surrogate/sampling/tests/test_samOptimalLHC.py
+++ surrogate/sampling/tests/test_samOptimalLHC.py
@@ -1,8 +1,8 @@
 from surrogate.sampling.samOptimalLHC import samOptimalLHC
 
 print '\nTest.sampling: samOptimalLHC'
-n = 1
-k = 4
+n = 100
+k = 3
 print '\tInput:  n=\t' + '\t' + str(n) + ''
 print '\tInput:  k=\t' + '\t' + str(k) + ''
 X = samOptimalLHC(n=n, k=k, population=30, iterations=30, generation=False)
