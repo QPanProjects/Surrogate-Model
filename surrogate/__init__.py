9a187bc34d06fa7958b8febb5f720ffb18cabc33
quanpan <quanpan302@hotmail.com>
2016-12-07 15:48:23 +0100

    Surrogate Model surrogate

--- surrogate/__init__.py
+++ surrogate/__init__.py
@@ -1,3 +1,9 @@
+"""Surrogate Model & Machine Learning
+Links:
+    http://www.kdnuggets.com/2015/06/top-20-python-machine-learning-open-source-projects.html
+
+"""
+
 
 __author__ = "Quan Pan"
 __version__ = "1.0"
