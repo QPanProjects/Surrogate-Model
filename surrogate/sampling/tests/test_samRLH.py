30352b4fdb35f28eb4170d2eb74923a36e269dd9
quanpan <quanpan302@hotmail.com>
2016-12-07 16:27:56 +0100

    Surrogate Model sampling

--- surrogate/sampling/tests/test_samRLH.py
+++ surrogate/sampling/tests/test_samRLH.py
@@ -1,8 +1,8 @@
 from surrogate.sampling.samRLH import samRLH
 
 print '\nTest.sampling: samRLH'
-n = 2
-k = 20
+n = 100
+k = 3
 print '\tInput:  n=\t' + '\t' + str(n) + ''
 print '\tInput:  k=\t' + '\t' + str(k) + ''
 X = samRLH(n=n, k=k)
