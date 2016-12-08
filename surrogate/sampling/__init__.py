f37d0bdde83147828aba13b96aa6f9cc82783329
quanpan <quanpan302@hotmail.com>
2016-12-08 11:19:59 +0100

    Surrogate Model sampling

--- surrogate/sampling/__init__.py
+++ surrogate/sampling/__init__.py
@@ -1,4 +1,4 @@
-"""Sampling Strategy
+"""Sampling Strategy, Experimental Design
 Links:
     https://www.google.nl/search?sclient=psy-ab&client=safari&rls=en&q=github+sampling+python&oq=github+sampling+python&gs_l=serp.3..0i22i30k1l2.2824.3846.0.4517.7.7.0.0.0.0.91.551.7.7.0....0...1c.1.64.psy-ab..0.7.538...33i160k1.G42G3jxX1XY&pbx=1&biw=1680&bih=961&dpr=2&cad=cbv&bvch=u&sei=-e5HWNLGHsrNgAbDjruoAg#q=github+sampling+strategy+python
 
@@ -6,6 +6,7 @@ Links:
 
     https://en.wikipedia.org/wiki/Latin_hypercube_sampling
 
+    https://docs.scipy.org/doc/numpy/reference/routines.random.html
 """
 
 
