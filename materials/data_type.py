1e6f7738f7b3ee0cd6c8c71822ee83703a06462a
quanpan <quanpan302@hotmail.com>
2016-12-09 17:34:31 +0100

    Surrogate Model materials.data_type

---
+++ materials/data_type.py
@@ -0,0 +1,43 @@
+"""Datatypes
+https://docs.python.org/2/library/stdtypes.html
+    5.1. Truth Value Testing
+    5.2. Boolean Operations
+    5.3. Comparisons
+    5.4. Numeric Types
+    5.5. Iterator Types
+    5.6. Sequence Types
+    5.7. Set Types
+    5.8. Mapping Types
+    5.9. File Objects
+    5.10. memoryview type
+    5.11. Context Manager Types
+    5.12. Other Built-in Types
+        1. Modules
+        2. Classes and Class Instances
+        3. Functions
+        4. Methods
+        5. Code Objects
+        6. Type Objects
+        7. The Null Object
+        8. The Ellipsis Object
+        9. The NotImplemented Object
+        10. Boolean Values
+        11. Internal Objects
+    5.13. Special Attributes
+        object.__dict__
+        object.__methods__
+        object.__members__
+        instance.__class__
+        class.__bases__
+        definition.__name__
+        class.__mro__
+        class.mro()
+        class.__subclasses__()
+
+The command numpy.asarray will turn a number of pre-set iterable containers (list, tuple, etc) into a numpy array.
+
+Links:
+    https://docs.python.org/2/library/datatypes.html
+    http://stackoverflow.com/questions/2402575/convert-list-in-tuple-to-numpy-array
+    http://stackoverflow.com/questions/28176949/convert-list-of-tuples-to-structured-numpy-array
+"""
