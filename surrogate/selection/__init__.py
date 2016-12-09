d96fac62a6d3621756d55f8d396c19e6474a3708
quanpan <quanpan302@hotmail.com>
2016-12-09 15:54:19 +0100

    Surrogate Model selection

--- surrogate/selection/__init__.py
+++ surrogate/selection/__init__.py
@@ -1,12 +1,15 @@
 from .selBest import selBest
+from .selDoubleTournament import selDoubleTournament
 from .selRandom import selRandom
 from .selRoulette import selRoulette
+from .selStochasticUniversalSampling import selStochasticUniversalSampling
 from .selTournament import selTournament
 from .selWorst import selWorst
 
 __all__ = [
     'selRandom',
     'selBest', 'selWorst',
-    'selTournament',
+    'selTournament', 'selDoubleTournament',
+    'selStochasticUniversalSampling',
     'selRoulette'
 ]
