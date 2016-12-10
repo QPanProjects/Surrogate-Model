a59a7155c9edacc96c67508ef455e67a694cd24e
quanpan <quanpan302@hotmail.com>
2016-12-10 18:11:57 +0100

    Surrogate Model example.anga

--- examples/anga.py
+++ examples/anga.py
@@ -52,51 +52,77 @@ Implementation:
 
 
 import warnings
-
 warnings.filterwarnings(action="ignore", category=Warning)
+# warnings.filterwarnings(action="ignore", category=FutureWarning)
+# warnings.filterwarnings(action="ignore", category=ImportWarning)
+# warnings.filterwarnings(action="ignore", category=DeprecationWarning)
+
 
 from surrogate.base import Individual
-from surrogate.sampling import samRandom
+from surrogate.sampling import samRandom, samBeta
 from surrogate.estimator import ANNSurrogate
 from surrogate.benchmarks import zdt6
 
+
 if __name__ == "__main__":
     _INF = 1e-14
+    _Ndim = 10
 
 
-    # def ObjFunction(fun, *args, **kwargs):
-    #     return fun(*args, **kwargs)
+    # Xold_ind = [[0., 0., 0., 0.], [1., 1., 1., 1.], [10., 10., 10., 10.]]
+    # # Yold_obj = [0.0, 1.0, 10.0]
+    # Yold_obj = [[0., 0.], [1., 1.], [10., 10.]]
+    # Xnew_ind = [[5., 5., 5., 5.], [-10., -2., -10., -2.]]
 
     # def Population(n=10, samStrategry):
     def Population(n=10):
         Individuals = []
-        for i in range(n):
+        estimator = zdt6
+        for i in range(_Ndim):
             variable = samRandom(n=10)
             # variable = samBeta(a=0.1, b=0.1, size=10)
-
-            # objective = 'zdt6_fun'
-            objective = zdt6(variable)
-
-            constrain = []
-
-            fitness = 10.0 - i
-            # fitness = _INF
-
-            Individuals.append(Individual(variable, objective, constrain, fitness))
+            objective = estimator(variable)
+            constraint = []
+            fitness = _INF
+            Individuals.append(Individual(estimator, variable, objective, constraint))
         return Individuals
 
 
+    def moeaLoop(surrogate, population):
+        Xold_ind = []
+        Yold_obj = []
+        Xnew_ind = samBeta(a=0.1, b=0.1, size=_Ndim)
+        for i in range(_Ndim):
+            population[i].objective = population[i].estimator(population[i].variable)
+
+            Xold_ind.append(population[i].variable)
+            Yold_obj.append(population[i].objective)
+            print str(i) + '.Xold_ind: [' + ', '.join(
+                map("{:.5f}".format, population[i].variable)) + ']' + '\tYold_obj: [' + ', '.join(
+                map("{:.5f}".format, population[i].objective)) + ']'
+        surrogate.fit(Xold_ind, Yold_obj)
+        Ynew_obj = surrogate.predict(Xnew_ind)
+        # print '  Xnew_ind:\t['+'\t'.join(['{:.5f}'.format(num) for num in Xnew_ind])+']'+'\tYnew_obj:\t['+'\t'.join(['{:.5f}'.format(num) for num in Ynew_obj])+']'
+        print 'ANNSurrogate.Xnew_ind:\n\t[' + '\t'.join(map(str, Xnew_ind)) + ']'
+        print 'ANNSurrogate.Ynew_obj:\n\t[' + '\t'.join(map(str, Ynew_obj)) + ']'
+
+
     population = Population()
-
-    Xold_ind = [[0., 0.], [1., 1.], [10., 10.]]
-    Yold_obj = [0.0, 1.0, 10.0]
-    Xnew_ind = [[5., 5.], [-10., -2.]]
-
+    print 'zdt6(samRandom(n=' + str(_Ndim) + ')):\n\t[' + '\t'.join(map(str, zdt6(samRandom(n=_Ndim)))) + ']'
+    print 'parentPpopulation[0].estimator(samRandom(n=' + str(_Ndim) + ')):\n\t[' + '\t'.join(
+        map(str, population[0].estimator(samRandom(n=_Ndim)))) + ']'
+    print
     surrogate = ANNSurrogate(algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-    surrogate.fit(Xold_ind, Yold_obj)
-    y_pred = surrogate.predict(Xnew_ind)
-    # print surrogate.regressor
-    print y_pred
+
+    moeaLoop(surrogate, population)
+
+    """Test Block
+    """
+    # surrogate = ANNSurrogate(algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    # surrogate.fit(Xold_ind, Yold_obj)
+    # y_pred = surrogate.predict(Xnew_ind)
+    # # print surrogate.regressor
+    # print 'ANNSurrogate.y_pred: ['+', '.join(map(str,y_pred))+']'
 
 
     # from sklearn.neural_network import MLPRegressor
@@ -104,4 +130,4 @@ if __name__ == "__main__":
     # regressor.fit(Xold_ind, Yold_obj)
     # y_pred = regressor.predict(Xnew_ind)
     # # print regressor
-    # print y_pred
+    # print 'MLPRegressor.y_pred: ['+', '.join(map(str,y_pred))+']'
