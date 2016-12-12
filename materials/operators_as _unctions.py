7a66f1be8d75113c5dfc2df47b1b9efb19db1c62
quanpan <quanpan302@hotmail.com>
2016-12-12 18:34:48 +0100

    Surrogate Model operators

---
+++ materials/operators_as _unctions.py
@@ -0,0 +1,14 @@
+"""
+Links:
+    https://docs.python.org/2/library/operator.html
+
+operator.
+    lt(a, b)    __lt__(a, b)
+    le(a, b)    __le__(a, b)
+    eq(a, b)    __eq__(a, b)
+    ne(a, b)    __ne__(a, b)
+    ge(a, b)    __ge__(a, b) a >= b
+    gt(a, b)    __gt__(a, b) a > b
+
+    mul(a, b)   __mul__(a, b)   Return a * b, for a and b numbers.
+"""
