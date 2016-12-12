6684766a0674696d759790fba5d7ac4f722ec95d
quanpan <quanpan302@hotmail.com>
2016-12-12 14:59:06 +0100

    Surrogate Model selection add NSGA2, SPEA2, and TournamentDCD

--- surrogate/selection/__init__.py
+++ surrogate/selection/__init__.py
@@ -1,15 +1,19 @@
 from .selBest import selBest
 from .selDoubleTournament import selDoubleTournament
+from .selNSGA2 import selNSGA2
 from .selRandom import selRandom
 from .selRoulette import selRoulette
+from .selSPEA2 import selSPEA2
 from .selStochasticUniversalSampling import selStochasticUniversalSampling
 from .selTournament import selTournament
+from .selTournamentDCD import selTournamentDCD
 from .selWorst import selWorst
 
 __all__ = [
     'selRandom',
     'selBest', 'selWorst',
-    'selTournament', 'selDoubleTournament',
+    'selTournament', 'selDoubleTournament', 'selTournamentDCD',
     'selStochasticUniversalSampling',
-    'selRoulette'
+    'selRoulette',
+    'selNSGA2', 'selSPEA2'
 ]
