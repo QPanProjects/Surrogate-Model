3c3cbf56fb70a3ac30eda7f321ec7a0b9359ff35
quanpan <quanpan302@hotmail.com>
2017-02-06 14:04:13 +0100

    Update web GUI design

--- examples/files/index.php
+++ examples/files/index.php
@@ -1,20 +1,250 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Taihu DSS</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 
     <style>
+    body {
+        width: 100%;
+        height: 100%;
+        font-family: 'Lato',sans-serif;
+        font-weight: 300;
+        color: #666;
+        background-color: #fff;
+    }
+
+    html {
+        width: 100%;
+        height: 100%;
+    }
+
+    .navbar {
+        margin-bottom: 0;
+        border-bottom: 1px solid rgba(255,255,255,.3);
+
+    }
+    .navbar-brand {
+        font-weight: 700;
+    }
+    .navbar-brand {
+        height: 40px;
+        padding: 5px 15px;
+        font-size: 18px;
+        line-height: 1em;
+    }
+    .navbar-brand h1{
+        color: #0D47A1;
+        font-size: 20px;
+        line-height: 40px;
+    }
+    .navbar-brand:focus {
+        outline: 0;
+    }
+    .nav.navbar-nav {
+        background-color: rgba(255,255,255,.4);
+    }
+    .navbar-custom.top-nav-collapse .nav.navbar-nav {
+        background-color: rgba(0,0,0,0);
+    }
+    .navbar-custom ul.nav li a {
+        font-size: 12px;
+        letter-spacing: 1px;
+        color: #1565C0;
+        text-transform: uppercase;
+        font-weight: 700;
+    }
+    .navbar-custom.top-nav-collapse ul.nav li a {
+        -webkit-transition: all .2s ease-in-out;
+        -moz-transition: all .2s ease-in-out;
+        transition: all .2s ease-in-out;
+        color: #1976D2;
+    }
+    .navbar-custom ul.nav ul.dropdown-menu {
+        border-radius: 0;
+    }
+    .navbar-custom ul.nav ul.dropdown-menu li {
+        border-bottom: 1px solid #f5f5f5;
+    }
+    .navbar-custom ul.nav ul.dropdown-menu li:last-child{
+        border-bottom: none;
+    }
+    .navbar-custom ul.nav ul.dropdown-menu li a {
+        padding: 10px 20px;
+        color: #1976D2;
+    }
+    .navbar-custom ul.nav ul.dropdown-menu li a:hover {
+        background: #fefefe;
+    }
+    .navbar-custom.top-nav-collapse ul.nav ul.dropdown-menu li a {
+        color: #1E88E5;
+    }
+    .navbar-custom .nav li a {
+        -webkit-transition: background .3s ease-in-out;
+        -moz-transition: background .3s ease-in-out;
+        transition: background .3s ease-in-out;
+    }
+    .navbar-custom .nav li a:hover,
+    .navbar-custom .nav li a:focus,
+    .navbar-custom .nav li.active {
+        outline: 0;
+        background-color: rgba(227,242,253,.4);
+    }
+    .navbar-custom.top-nav-collapse .nav li a:hover,
+    .navbar-custom.top-nav-collapse .nav li a:focus,
+    .navbar-custom.top-nav-collapse .nav li.active {
+        outline: 0;
+        background-color: rgba(227,242,253,.4);
+    }
+    .navbar-toggle {
+        padding: 4px 6px;
+        font-size: 14px;
+        color: #0D47A1;
+    }
+    .navbar-toggle:focus,
+    .navbar-toggle:active {
+        outline: 0;
+    }
+
+    /* ===========================
+     * Elements
+     */
+    .btn {
+        border-radius: 0;
+        text-transform: uppercase;
+        font-family: Montserrat,sans-serif;
+        font-weight: 400;
+        -webkit-transition: all .3s ease-in-out;
+        -moz-transition: all .3s ease-in-out;
+        transition: all .3s ease-in-out;
+    }
+    .btn-circle {
+        width: 70px;
+        height: 70px;
+        margin-top: 15px;
+        padding: 7px 16px;
+        border: 2px solid #fff;
+        border-radius: 50%;
+        font-size: 40px;
+        color: #fff;
+        background: 0 0;
+        -webkit-transition: background .3s ease-in-out;
+        -moz-transition: background .3s ease-in-out;
+        transition: background .3s ease-in-out;
+    }
+    .btn-circle.btn-dark {
+        border: 2px solid #666;
+        color: #666;
+    }
+    .btn-circle:hover,
+    .btn-circle:focus {
+        outline: 0;
+        color: #fff;
+        background: rgba(255,255,255,.1);
+    }
+    .btn-circle.btn-dark :hover,
+    .btn-circle.btn-dark :focus {
+        outline: 0;
+        color: #999;
+        background: #fff;
+    }
+    .btn-circle.btn-dark :hover i,
+    .btn-circle.btn-dark :focus i{
+        color: #999;
+    }
+
+    .page-scroll .btn-circle i.animated {
+        -webkit-transition-property: -webkit-transform;
+        -webkit-transition-duration: 1s;
+        -moz-transition-property: -moz-transform;
+        -moz-transition-duration: 1s;
+    }
+    .page-scroll .btn-circle:hover i.animated {
+        -webkit-animation-name: pulse;
+        -moz-animation-name: pulse;
+        -webkit-animation-duration: 1.5s;
+        -moz-animation-duration: 1.5s;
+        -webkit-animation-iteration-count: infinite;
+        -moz-animation-iteration-count: infinite;
+        -webkit-animation-timing-function: linear;
+        -moz-animation-timing-function: linear;
+    }
+    @-webkit-keyframes pulse {
+        0 {
+            -webkit-transform: scale(1);
+            transform: scale(1);
+        }
+        50% {
+            -webkit-transform: scale(1.2);
+            transform: scale(1.2);
+        }
+        100% {
+            -webkit-transform: scale(1);
+            transform: scale(1);
+        }
+    }
+    @-moz-keyframes pulse {
+        0 {
+            -moz-transform: scale(1);
+            transform: scale(1);
+        }
+        50% {
+            -moz-transform: scale(1.2);
+            transform: scale(1.2);
+        }
+        100% {
+            -moz-transform: scale(1);
+            transform: scale(1);
+        }
+    }
+
+    #preloader {
+        background: #ffffff;
+        bottom: 0;
+        height: 100%;
+        left: 0;
+        position: fixed;
+        right: 0;
+        top: 0;
+        width: 100%;
+        z-index: 999;
+    }
+    #load {
+        z-index: 9999;
+        background-color: #FF4136;
+        opacity: 0.75;
+        width: 10px;
+        height: 10px;
+        position: absolute;
+        top: 50%;
+        margin: -5px auto 0 auto;
+        left: 0;
+        right: 0;
+        border-radius: 0px;
+        border: 5px solid #FF4136;
+        box-shadow: 10px 0px #39CCCC, 10px 0px #01FF70;
+        animation: shadowSpin 1s ease-in-out infinite;
+    }
+
+    /* ===========================
+     * self design
+     */
     .noresize {
         resize: none;
+    }
+    a:hover, a:active, a:focus {
+        outline: 0;
     }
     .alert {
         margin: 10px 0px;
     }
+
     .jumbotron {
         width:100%;
         height:100%;
@@ -27,57 +257,70 @@
         background-size:cover !important;
         background-position:center !important;
     }
-    footer {
-        background-color: #f2f2f2;;
+
+    .section {
+        padding-top:50px;
+        height:400px;
         color: #000000;
+        background-color: #eeeeee;
+    }
+
+    /* ===========================
+     * Footer
+     */
+    footer {
+        text-align: center;
+        background-color: #f2f2f2;;
         margin-top: 25px;
         padding: 15px;
+    }
+    footer p {
+        margin-top: 25px;
+        color: #000000;
     }
     </style>
 
     <script>
-        var serverTime = new Date();
-
-        function updateTime() {
-            serverTime = new Date(serverTime.getTime() + 1000);
-            $('#gltimer').html(serverTime.toGMTString());
-        }
-        updateTime();
-        setInterval(updateTime, 1000);
     </script>
 </head>
-<body>
-<nav class="navbar navbar-default navbar-fixed-top">
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+<div id="preloader">
+    <div id="load"></div>
+</div>
+
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
     <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">DSS</a>
+            <a class="navbar-brand" href="/">
+                <h1>Home</h1>
+            </a>
         </div>
 
-        <div class="collapse navbar-collapse" id="myNavbar">
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="/taihu">Taihu</a></li>
-                <li><a href="/wsn">WSN</a></li>
+                <li class="active"><a href="#secdss">DSS</a></li>
+                <li><a href="#sectaihu">Taihu</a></li>
+                <li><a href="#secwsn">WSN</a></li>
+
+                <li><a href="#secdoc">Document</a></li>
+                <li><a href="#secabout">About</a></li>
 
                 <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Test<span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Test <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><a href="/moea/test_amga.php" target="_blank">AMGA2</a></li>
                         <li><a href="/moea/test_nsga.php" target="_blank">NSGAII</a></li>
                     </ul>
                 </li>
             </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="/docs/_build/html/" target="_blank">Document</a></li>
-                <li><a href="https://nl.linkedin.com/in/quanpan302" target="_blank">About</a></li>
-                <p id="gltimer" class="navbar-text"><i class="fa fa-clock-o" aria-hidden="true"></i></p>
-            </ul>
         </div>
+
     </div>
 </nav>
 
@@ -86,36 +329,188 @@
     <div class="jumbotron">
         <h1>Decision Support System</h1>
         <p>
-            Quan Pan&nbsp;
             <a href="https://nl.linkedin.com/in/quanpan302" target="_blank">
-                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>&nbsp;Quan Pan
             </a>
         </p>
     </div>
 
     <div class="row">
         <div class="col-sm-12">
-            <h2>Decision Support System</h2>
-        </div>
-        <div class="col-sm-12">
-            <h2>Taihu Lake</h2>
-            <p>
-                Taihu Lake DSS
-                <a href="/taihu">
-                    <i class="fa fa-link" aria-hidden="true"></i>
-                </a>
-            </p>
-        </div>
-        <div class="col-sm-12">
-            <h2>Water Supply Network</h2>
-            <p>
-                Water Supply Network DSS
-                <a href="/wsn">
-                    <i class="fa fa-link" aria-hidden="true"></i>
-                </a>
-            </p>
+            <p id="gltimer" class="text-right"><i class="fa fa-clock-o" aria-hidden="true"></i></p>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <section id="secdss" class="section text-center">
+                <h2>Decision Support System</h2>
+                <p>
+                    <a href="/moea/test_amga.php" target="_blank">
+                        <i class="fa fa-link animated" aria-hidden="true"></i>&nbsp;AMGA2
+                    </a>
+                </p>
+                <p>
+                    <a href="/moea/test_nsga.php" target="_blank">
+                        <i class="fa fa-link animated" aria-hidden="true"></i>&nbsp;NSGAII
+                    </a>
+                </p>
+                <div class="page-scroll">
+                    <a href="#secabout" class="btn btn-circle btn-dark">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
+                </div>
+            </section>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <section id="sectaihu" class="section text-center">
+                <h2>Taihu Lake</h2>
+                <p>
+                    <a href="/taihu">
+                        <i class="fa fa-link animated" aria-hidden="true"></i>&nbsp;Taihu Lake DSS
+                    </a>
+                </p>
+            </section>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <section id="secwsn" class="section text-center">
+                <h2>Water Supply Network</h2>
+                <p>
+                    <a href="/wsn">
+                        <i class="fa fa-link animated" aria-hidden="true"></i>&nbsp;Water Supply Network DSS
+                    </a>
+                </p>
+            </section>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <section id="secdoc" class="section text-center">
+                <h2>Document</h2>
+                <p>
+                    <a href="/docs/_build/html/" target="_blank">
+                        <i class="fa fa-link animated" aria-hidden="true"></i>&nbsp;Document
+                    </a>
+                </p>
+            </section>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <section id="secabout" class="section text-center">
+                <h2>About</h2>
+                <p>
+                    <a href="/index_about.php">
+                        <i class="fa fa-link animated" aria-hidden="true"></i>&nbsp;About
+                    </a>
+                </p>
+            </section>
+        </div>
+    </div>
+
 </div>
 
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="wow shake" data-wow-delay="0.4s">
+                    <div class="page-scroll">
+                        <a href="#secdss" id="totop" class="btn btn-circle btn-dark">
+                            <i class="fa fa-angle-double-up animated"></i>
+                        </a>
+                    </div>
+                </div>
+                <p>
+                    <i class="fa fa-copyright" aria-hidden="true"></i>&nbsp;All rights reserved.&nbsp;
+                    <i class="fa fa-id-card" aria-hidden="true"></i>&nbsp;Quan Pan&nbsp;
+                    <a href="https://nl.linkedin.com/in/quanpan302" target="_blank">
+                        <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.0/jquery.scrollTo.min.js"></script>
+<script src="/assets/js/wow.js"></script>
+
+<script>
+(function ($) {
+    new WOW().init();
+
+    $(window).load(function() {
+        // executes when complete page is fully loaded,
+        // including all frames, objects and images
+
+        $("#preloader").delay(100).fadeOut("slow");
+        $("#load").delay(100).fadeOut("slow");
+    });
+
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    $(function(){
+        // executes when HTML-Document is loaded and DOM is ready
+        // $(document).ready(function(){  });
+        // $(function(){  });
+
+        var serverTime = new Date();
+
+        function updateTime() {
+            serverTime = new Date(serverTime.getTime() + 1000);
+            $('#gltimer').html(serverTime.toGMTString());
+        }
+        updateTime();
+        setInterval(updateTime, 1000);
+
+        $('.navbar-nav li a[href^="#"]').on('click', function(event) {
+            // method 1
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 1000, function(){
+                    window.location.hash = hash;
+                });
+            }
+
+            // method 2
+            //  jquery easing plugin
+            //just pass scrollclass in design and start scrolling
+			//var $anchor = $(this);
+			//$('html, body').stop().animate({
+			//	scrollTop: $($anchor.attr('href')).offset().top
+			//}, 1000, 'easeInOutExpo');
+			//event.preventDefault();
+		});
+		$('.page-scroll a[href^="#"]').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1000, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+})(jQuery);
+</script>
+</body>
+</html>
---
+++ examples/files/index_about.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Taihu DSS</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <style>
+    .noresize {
+        resize: none;
+    }
+    .alert {
+        margin: 10px 0px;
+    }
+    .jumbotron {
+        width:100%;
+        height:100%;
+        height:calc(100% - 1px);
+        background-image:url('/assets/img/taihu-feature-graphic.png');
+        background-repeat:no-repeat !important;
+        -webkit-background-size:cover !important;
+        -moz-background-size:cover !important;
+        -o-background-size:cover !important;
+        background-size:cover !important;
+        background-position:center !important;
+    }
+    footer {
+        background-color: #f2f2f2;;
+        color: #000000;
+        margin-top: 25px;
+        padding: 15px;
+    }
+    </style>
+
+    <script>
+        var serverTime = new Date();
+
+        function updateTime() {
+            serverTime = new Date(serverTime.getTime() + 1000);
+            $('#gltimer').html(serverTime.toGMTString());
+        }
+        updateTime();
+        setInterval(updateTime, 1000);
+    </script>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Home</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="/taihu">Taihu</a></li>
+                <li><a href="/wsn">WSN</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/docs/_build/html/" target="_blank">Document</a></li>
+                <li class="active"><a href="/index_about.php">About</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>Water Supply Network</h1>
+        <p>
+            Quan Pan&nbsp;
+            <a href="https://nl.linkedin.com/in/quanpan302" target="_blank">
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+        </p>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <p id="gltimer" class="text-right"><i class="fa fa-clock-o" aria-hidden="true"></i></p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="alert alert-info">About</div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+
+
+<?php
+    echo 'Author: Quan Pan'
+?>
+        </div>
+    </div>
+</div>
+<footer class="container-fluid text-center">
+    <span>Quan Pan&nbsp;</span>
+    <a href="https://nl.linkedin.com/in/quanpan302" target="_blank">
+        <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+    </a>
+</footer>
+
+<script>
+
+</script>
+</body>
+</html>
