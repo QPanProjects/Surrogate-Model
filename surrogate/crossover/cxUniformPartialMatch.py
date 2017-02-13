54f562200da1f844210d715622e50ea60d19355c
quanpan <quanpan302@hotmail.com>
2017-02-13 16:25:35 +0100

    Add MIT License header

--- surrogate/crossover/cxUniformPartialMatch.py
+++ surrogate/crossover/cxUniformPartialMatch.py
@@ -1,3 +1,29 @@
+# MIT License
+#
+# Copyright (c) 2016 Daily Actie
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Author: Quan Pan <quanpan302@hotmail.com>
+# License: MIT License
+# Create: 2016-12-02
+
 """Executes a uniform partially matched crossover (UPMX) on the input
 individuals. The two individuals are modified in place. This crossover
 expects :term:`sequence` individuals of indices, the result for any other
@@ -19,8 +45,6 @@ functions from the python base :mod:`random` module.
    control parameter optimization", 2000.
 """
 
-
-# Authors: Quan Pan <quanpan302@hotmail.com>
 
 import random
 
